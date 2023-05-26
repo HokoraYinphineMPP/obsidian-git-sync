147e9f1e8eba4e060fea8805b13c37f48e7f6f67
HokoraYinphineMPP <holowisewolfmpp@gmail.com>
2023-05-26 05:16:30 -0700

    vault backup: 2023-05-26 05:16:29

--- updateVer.py
+++ updateVer.py
@@ -9,9 +9,12 @@ class MyPlugin(BasePlugin):
         # Extract the dynamic part of the asset name
         for asset in data['assets']:
             if asset['name'].startswith('revanced-manager') and asset['name'].endswith('.apk'):
-                self.dynamic_part = asset['name'][len('revanced-manager'):-len('.apk')]
+                dynamic_part = asset['name'][len('revanced-manager'):-len('.apk')]
                 break
+        # Generate the link to the latest release
+        self.latest_release_link = f'https://github.com/owner/repository/releases/latest/download/revanced-manager{dynamic_part}.apk'
 
-    def on_page_context(self, context, page, config, nav):
-        # Pass the dynamic part of the asset name to the Jinja template
-        context['dynamic_part'] = self.dynamic_part
+    def on_page_markdown(self, markdown, page, config, files):
+        # Inject the link to the latest release into the Markdown content
+        markdown = markdown.replace('{{ latest_release_link }}', self.latest_release_link)
+        return markdown
