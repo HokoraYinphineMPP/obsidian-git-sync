9f122df9134faad64e3d8af459caf858876c9c2c
bogadana <git@bogadana.com>
2023-05-26 15:20:59 +0200

    improve python

--- get_latest_manager_release/get_latest_manager_release.py
+++ get_latest_manager_release/get_latest_manager_release.py
@@ -3,19 +3,13 @@ import requests
 
 class GetLatestManagerRelease(BasePlugin):
     def on_pre_build(self, config):
-        # Make an API call to the GitHub API to get information about release assets
-        response = requests.get('https://api.github.com/repos/revanced/revanced-manager/releases/latest')
-        data = response.json()
-        # Extract the dynamic part of the asset name
-        for asset in data['assets']:
-            if asset['name'].startswith('revanced-manager') and asset['name'].endswith('.apk'):
-                dynamic_part = asset['name'][len('revanced-manager'):-len('.apk')]
-                break
-        # Generate the link to the latest release
-        self.latest_release_link = f'https://github.com/revanced/revanced-manager/releases/latest/download/revanced-manager{dynamic_part}.apk'
+        repo = 'revanced/revanced-manager'
+
+        json = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()
+        manager_apk = next(asset['name'] for asset in json['assets'] if asset['name'].startswith('revanced-manager') and asset['name'].endswith('.apk'))
+
+        self.latest_release_link = f'https://github.com/{repo}/releases/latest/download/{manager_apk}'
 
     def on_page_markdown(self, markdown, page, config, files):
-        # Inject the link to the latest release into the Markdown content
-        markdown = markdown.replace('{{ latest_release_link }}', self.latest_release_link)
-        return markdown
+        return markdown.replace('{{ latest_release_link }}', self.latest_release_link)
 
